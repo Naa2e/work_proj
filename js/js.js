9e039f969c491e6b1841a0dfc20001f5cc21720a
Nicole Ahima <Nicole.Ahima@outlook.com>
2015-06-17 20:37:14 -0500

    first route

--- js/js.js
+++ js/js.js
@@ -1,11 +1,4 @@
 var app = angular.module('servicesApp', ['ngRoute']);
 
-app.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    .when("/", {templateUrl: "partials/home.html", controller: "MainCtrl"})
-    .when("/conditions", {templateUrl: "partials/CBD.html", controller: "ConditionsCtrl"})
-    .when('/webcams', {templateUrl:'partials/webcams.html', controller:'WebcamsCtrl'})
-    .when('/tenday', {templateUrl:'partials/tenday.html', controller:'TendayCtrl'})
-    .otherwise("/404", {templateUrl: "partials/404.html", controller: "ErrCtrl"});
-}]);
+
 
