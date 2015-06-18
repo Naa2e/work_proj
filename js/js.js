011aa7eee45ebea7c162768582466e9ae4ec72e3
Nicole Ahima <Nicole.Ahima@outlook.com>
2015-06-17 20:57:04 -0500

    May break soon

--- js/js.js
+++ js/js.js
@@ -1,4 +1,9 @@
 var app = angular.module('servicesApp', ['ngRoute']);
 
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when("/", {templateUrl: "partials/home.html", controller: "MainCtrl"});
 
+}]);
 
+app.constant('API_URL', 'https://residential.firebaseio.com');
